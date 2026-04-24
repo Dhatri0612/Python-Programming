6956a8ea894cb790eb580511a5d2d9388dd4adc4
Dhatri0612 <tripathi.dhatri0623@gmail.com>
2026-04-24 17:26:53 +0530

    Fixed employee inheritance code

--- inheritance/single_inheritance/employee.py
+++ inheritance/single_inheritance/employee.py
@@ -1,15 +1,23 @@
 class Employee:
-    def __init__(self,name,salary):
-        self.name=name
-        self.salary=salary
-    def show_details(self):
-        print("Name: ",self.name)
-        print("Salary: ",self.salary)
-class Manager(Employee):
-    def __init__(self,bonus):
-        super().__init(name,salary)
-        self.bonus=bonus
-    def total_Salary(self):
-        print("total salary: ",salary+bonus)
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
 
-m=Manager()
+    def show_details(self):
+        print("Name:", self.name)
+        print("Salary:", self.salary)
+
+
+class Manager(Employee):
+    def __init__(self, name, salary, bonus):
+        super().__init__(name, salary)
+        self.bonus = bonus
+
+    def total_salary(self):
+        print("Total Salary:", self.salary + self.bonus)
+
+
+m = Manager("Rahul", 50000, 10000)
+
+m.show_details()
+m.total_salary()
